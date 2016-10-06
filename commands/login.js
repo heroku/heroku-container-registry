ffa0b2f64fa8e7aadc44e4ff523f41efc459a32a
Kawajiri Ryoma <ryoma.edison@gmail.com>
2016-10-07 08:22:58 +0900

    Switch docker login args by docker verison

--- commands/login.js
+++ commands/login.js
@@ -3,6 +3,7 @@
 const cli = require('heroku-cli-util');
 const co = require('co');
 const child = require('child_process');
+const semver = require('semver');
 
 module.exports = function(topic) {
   return {
@@ -30,20 +31,47 @@ function* login(context, heroku) {
 }
 
 function dockerLogin(registry, password, verbose) {
+  return dockerVersion().then((version) => {
+    return new Promise((resolve, reject) => {
+      let args = [
+        'login',
+        '--username=_',
+        `--password=${ password }`,
+        registry
+      ];
+      if (semver.lte(version, "1.10.0")) {
+        args.splice(1, 0, '--email=_')
+      }
+      if (verbose) {
+        console.log(['> docker'].concat(args).join(' '));
+      }
+      child.spawn('docker', args, { stdio: 'inherit' })
+        .on('exit', (code, signal) => {
+          if (signal || code) reject(signal || code);
+          else resolve();
+        });
+    });
+  });
+}
+
+function dockerVersion() {
   return new Promise((resolve, reject) => {
     let args = [
-      'login',
-      '--username=_',
-      `--password=${ password }`,
-      registry
+      'version',
+      '--format',
+      "'{{.Server.Version}}'"
     ];
-    if (verbose) {
-      console.log(['> docker'].concat(args).join(' '));
-    }
-    child.spawn('docker', args, { stdio: 'inherit' })
-      .on('exit', (code, signal) => {
-        if (signal || code) reject(signal || code);
-        else resolve();
-      });
+
+    const version = child.spawn('docker', args, {})
+    version.stdout.on('data', (data) => {
+      if (data) {
+        resolve(data.toString().slice(1, -1));
+      } else {
+        reject(new Error("Not output"));
+      }
+    });
+    version.on('exit', (code, signal) => {
+      if (signal || code) reject(signal || code);
+    });
   });
 }
