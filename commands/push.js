502e3f1aeb5065dd4fcdbbcc0fafd606ffb1d1f5
Matthew Origer <moriger@heroku.com>
2017-05-03 09:53:58 -0500

    multiple dockerfiles
    use inquirer to pick

--- commands/push.js
+++ commands/push.js
@@ -1,16 +1,8 @@
-'use strict';
+const cli = require('heroku-cli-util')
+const co = require('co')
+let Sanbashi = require('../lib/sanbashi')
 
-const cli = require('heroku-cli-util');
-const co = require('co');
-const child = require('child_process');
-const log = require('../lib/log');
-const fs = require('fs');
-const path = require('path');
-const glob = require('glob');
-
-const DOCKERFILE_REGEX = /\/Dockerfile(.\w*)?$/;
-
-module.exports = function(topic) {
+module.exports = function (topic) {
   return {
     topic: topic,
     command: 'push',
@@ -19,126 +11,53 @@ module.exports = function(topic) {
     needsAuth: true,
     variableArgs: true,
     flags: [
-      { name: 'verbose', char: 'v', hasValue: false }
+      {
+        name: 'verbose',
+        char: 'v',
+        hasValue: false
+      },
+      {
+        name: 'recursive',
+        char: 'R',
+        hasValue: false
+      }
     ],
     run: cli.command(co.wrap(push))
-  };
-};
+  }
+}
 
-function* push(context, heroku) {
-  let herokuHost = process.env.HEROKU_HOST || 'heroku.com';
-  let registry = `registry.${ herokuHost }`;
-  let dockerfiles = getDockerfiles(context.cwd, true);
-  let possibleJobs = getJobs(`${ registry }/${ context.app }`, context.args, dockerfiles);
-  let jobs = chooseJobs(possibleJobs);
-
+function* push (context, heroku) {
+  let herokuHost = process.env.HEROKU_HOST || 'heroku.com'
+  let registry = `registry.${ herokuHost }`
+  let dockerfiles = Sanbashi.getDockerfiles(process.cwd(), true)
+  let possibleJobs = Sanbashi.getJobs(`${ registry }/${ context.app }`, context.args, dockerfiles)
+  let jobs = yield Sanbashi.chooseJobs(possibleJobs)
   if (!jobs.length) {
-    cli.warn('No images to push');
-    process.exit();
+    cli.warn('No images to push')
+    process.exit()
   }
 
   try {
     for (let job of jobs) {
-      cli.log(`\n=== Building ${ job.name } (${ job.dockerfile })`);
-      yield buildImage(job.dockerfile, job.resource, context.flags.verbose);
+      cli.log(`\n=== Building ${ job.name } (${ job.dockerfile })`)
+      yield Sanbashi.buildImage(job.dockerfile, job.resource, context.flags.verbose)
     }
   }
   catch (err) {
-    cli.error(`Error: docker build exited with ${ err }`);
-    cli.hush(err.stack || err);
-    process.exit(1);
+    cli.error(`Error: docker build exited with ${ err }`)
+    cli.hush(err.stack || err)
+    process.exit(1)
   }
 
   try {
     for (let job of jobs) {
-      cli.log(`\n=== Pushing ${ job.name } (${ job.dockerfile })`);
-      yield pushImage(job.resource, context.flags.verbose);
+      cli.log(`\n=== Pushing ${ job.name } (${ job.dockerfile })`)
+      yield Sanbashi.pushImage(job.resource, context.flags.verbose)
     }
   }
   catch (err) {
-    cli.error(`Error: docker push exited with ${ err }`);
-    cli.hush(err.stack || err);
-    process.exit(1);
+    cli.error(`Error: docker push exited with ${ err }`)
+    cli.hush(err.stack || err)
+    process.exit(1)
   }
-}
-
-function buildImage(dockerfile, resource, verbose) {
-  return new Promise((resolve, reject) => {
-    let cwd = path.dirname(dockerfile);
-    let args = [ 'build', '-f', dockerfile, '-t', resource, cwd ];
-    log(verbose, args);
-    child.spawn('docker', args, { stdio: 'inherit' })
-      .on('exit', (code, signal) => {
-        if (signal || code) reject(signal || code);
-        else resolve();
-      });
-  });
-}
-
-function pushImage(resource, verbose) {
-  return new Promise((resolve, reject) => {
-    let args = [ 'push', resource ];
-    log(verbose, args);
-    child.spawn('docker', args, { stdio: 'inherit' })
-      .on('exit', (code, signal) => {
-        if (signal || code) reject(signal || code);
-        else resolve();
-      });
-  });
-}
-
-function getDockerfiles(dir, recursive) {
-  let match = recursive ? '**/Dockerfile?(.)*' : 'Dockerfile?(.)*';
-  let dockerfiles = glob.sync(match, { cwd: dir, nonull: false, nodir: true });
-  return dockerfiles.map(file => path.join(dir, file));
-}
-
-function getJobs(resourceRoot, procs, dockerfiles) {
-  return dockerfiles
-    // convert all Dockerfiles into job Objects
-    .map((dockerfile) => {
-      let match = dockerfile.match(DOCKERFILE_REGEX);
-      if (!match) return;
-      let proc = (match[1] || '.web').slice(1);
-      return {
-        name: proc,
-        resource: `${ resourceRoot }/${ proc }`,
-        dockerfile: dockerfile,
-        postfix: path.basename(dockerfile) === 'Dockerfile' ? 0 : 1,
-        depth: path.normalize(dockerfile).split(path.sep).length
-      };
-    })
-    // if process types have been specified, filter non matches out
-    .filter(job => {
-      return job && (!procs.length || procs.indexOf(job.name) !== -1);
-    })
-    // prefer closer Dockerfiles, then prefer Dockerfile over Dockerfile.web
-    .sort((a, b) => {
-      return a.depth - b.depth || a.postfix - b.postfix;
-    })
-    // group all Dockerfiles for the same process type together
-    .reduce((jobs, job) => {
-      jobs[job.name] = jobs[job.name] || [];
-      jobs[job.name].push(job);
-      return jobs;
-    }, {});
-}
-
-function chooseJobs(jobs) {
-  return Object.keys(jobs).map(name => {
-    let group = jobs[name];
-    if (group.length > 1) {
-      let ambiguous = group.map(job => job.dockerfile);
-      if (group[1].depth === group[0].depth) {
-        if (group[1].postfix === group[0].postfix) {
-          cli.error(`Cannot build.  More than one Dockerfile defines the process type:\n${ ambiguous.join('\n') }`);
-          process.exit(1);
-        }
-      }
-      cli.warn(`WARNING: Using nearest match for '${ group[0].name }' process type:`);
-      cli.warn(`WARNING: ${ ambiguous[0] } (used)`);
-      ambiguous.slice(1).forEach(file => cli.warn(`WARNING: ${ file } (ignored)`));
-    }
-    return group[0];
-  });
 }
---
+++ test/sanbashi.test.js
@@ -0,0 +1,82 @@
+let Sinon = require('sinon')
+let Sanbashi = require('../lib/sanbashi')
+let expect = require('chai').expect
+let Path = require('path')
+let Inquirer = require('inquirer')
+
+describe('Sanbashi', () => {
+  describe('.getDockerfiles', () => {
+    it('can find in just the given directory', () => {
+      const searchpath = Path.join(process.cwd(), './test/fixtures')
+      let results = Sanbashi.getDockerfiles(searchpath, false)
+      expect(results).to.have.members([`${searchpath}/Dockerfile.web`])
+    })
+    it('can recurse the directory', () => {
+      const searchpath = Path.join(process.cwd(), './test/fixtures')
+      let results = Sanbashi.getDockerfiles(searchpath, true)
+      expect(results).to.have.members([`${searchpath}/Dockerfile.web`, `${searchpath}/Nested/Dockerfile`])
+    })
+  })
+  describe('.getJobs', () => {
+    it('returns objects representing jobs per Dockerfile', () => {
+      const dockerfiles = ['./Dockerfile.web', './Nested/Dockerfile.web']
+      const resourceRoot = 'rootfulroot'
+      const results = Sanbashi.getJobs(resourceRoot, ['web'], dockerfiles)
+      expect(results.web).to.have.property('length', 2)
+      expect(results.web[0]).to.have.property('depth', 1, 'dockerfile', './Dockerfile.web', 'postfix', 1)
+      expect(results.web[1]).to.have.property('depth', 2, 'dockerfile', './Nested/Dockerfile.web', 'postfix', 1)
+    })
+    it('filters out by process type', () => {
+      const dockerfiles = ['./Dockerfile.web', './Nested/Dockerfile.worker']
+      const resourceRoot = 'rootfulroot'
+      const results = Sanbashi.getJobs(resourceRoot, ['web'], dockerfiles)
+      expect(results.web).to.have.property('length', 1)
+      expect(results.web[0]).to.have.property('name', 'web')
+      expect(results).to.not.have.property('worker')
+    })
+    it('sorts dockerfiles by directory depth, then proc type', () => {
+      const dockerfiles = ['./Nested/Dockerfile.worker', './Dockerfile.web', './Nested/Dockerfile']
+      const resourceRoot = 'rootfulroot'
+      const results = Sanbashi.getJobs(resourceRoot, [], dockerfiles)
+      expect(results.web).to.have.property('length', 2)
+      expect(results.web[0]).to.have.property('dockerfile', './Dockerfile.web')
+      expect(results.web[1]).to.have.property('dockerfile', './Nested/Dockerfile')
+      expect(results.worker[0]).to.have.property('dockerfile', './Nested/Dockerfile.worker')
+    })
+    it('groups the jobs by process type', () => {
+      const dockerfiles = ['./Nested/Dockerfile.worker', './Dockerfile.web', './Nested/Dockerfile']
+      const resourceRoot = 'rootfulroot'
+      const results = Sanbashi.getJobs(resourceRoot, [], dockerfiles)
+      expect(results).to.have.keys('worker', 'web')
+      expect(results['worker'].map(j => j.dockerfile)).to.have.members(['./Nested/Dockerfile.worker'])
+      expect(results['web'].map(j => j.dockerfile)).to.have.members(['./Dockerfile.web', './Nested/Dockerfile'])
+    })
+  })
+  describe('.chooseJobs', () => {
+    it('returns the entry when ony one exist', () => {
+      const dockerfiles = ['./Nested/Dockerfile.web']
+      const jobs = Sanbashi.getJobs('rootfulroot', [], dockerfiles)
+      let chosenJob = Sanbashi.chooseJobs(jobs)
+      expect(chosenJob[0]).to.have.property('dockerfile', dockerfiles[0])
+      expect(chosenJob).to.have.property('length',1)
+    })
+    it.skip('queries the user when more than one job of a type exists', () => {
+      //really no way to simulate user input for Inquirer
+      Sinon.stub(Inquirer, 'prompt').resolves('something')
+      const dockerfiles = ['./Nested/Dockerfile.web', './OtherNested/Dockerfile.web','./AnotherNested/Dockerfile.web', './Dockerfile.web']
+      let jobs = Sanbashi.getJobs('rootfulroot', ['web'], dockerfiles)
+      let res = Sanbashi.chooseJobs(jobs)
+      //   .then((chosen) => {
+      //     console.dir(chosen, {
+      //       colors: true,
+      //       depth: null
+      //     })
+      // })
+      console.dir(res)
+    })
+    afterEach(() => {
+      if(Inquirer.prompt.restore)
+        Inquirer.prompt.restore()
+    })
+  })
+})
