5c01eabba4f7cfc8144300c6bd3fb01fab8cbdc1
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-07-11 07:50:10 -0700

    add help topic

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var pkg = require('./package.json');
 
+
 module.exports = {
+  topic: {
+    topic: 'container',
+    description: 'heroku container registry'
+  },
   commands: [
     require('./commands/index')(pkg),
     require('./commands/login')(pkg.topic),
