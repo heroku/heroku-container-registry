dc0aed03a554155b40d88c267e0c63bb11a65b07
Jeff Dickey <dickeyxxx@users.noreply.github.com>
2017-07-11 07:48:55 -0700

    revert

--- commands/login.js
+++ commands/login.js
@@ -1,42 +1,49 @@
-const cli = require('heroku-cli-util')
-const log = require('../lib/log')
+'use strict';
 
-module.exports = function (topic) {
+const cli = require('heroku-cli-util');
+const co = require('co');
+const child = require('child_process');
+
+module.exports = function(topic) {
   return {
     topic: topic,
     command: 'login',
-    flags: [{name: 'verbose', char: 'v', hasValue: false}],
-    description: 'login to the Heroku Container Registry',
-    help: `Usage:
-        heroku container:login`,
+    flags: [{ name: 'verbose', char: 'v', hasValue: false }],
+    description: 'Logs in to the Heroku Docker registry',
     needsApp: false,
     needsAuth: true,
-    run: cli.command(login)
-  }
-}
+    run: cli.command(co.wrap(login))
+  };
+};
 
-async function login (context, heroku) {
-  let herokuHost = process.env.HEROKU_HOST || 'heroku.com'
-  let registry = `registry.${ herokuHost }`
-  let password = context.auth.password
+function* login(context, heroku) {
+  let herokuHost = process.env.HEROKU_HOST || 'heroku.com';
+  let registry = `registry.${ herokuHost }`;
+  let password = context.auth.password;
 
   try {
-    let user = await dockerLogin(registry, password, context.flags.verbose)
+    let user = yield dockerLogin(registry, password, context.flags.verbose);
   }
   catch (err) {
-    cli.error(`Error: docker login exited with ${ err }`)
-    cli.hush(err.stack || err)
-    cli.exit(1)
+    cli.error(`Error: docker login exited with ${ err }`);
   }
 }
 
-function dockerLogin (registry, password, verbose) {
-  let args = [
-    'login',
-    '--username=_',
-    `--password=${ password }`,
-    registry
-  ]
-  log(verbose, args)
-  return Sanbashi.cmd('docker', args)
+function dockerLogin(registry, password, verbose) {
+  return new Promise((resolve, reject) => {
+    let args = [
+      'login',
+      '--username=_',
+      `--password=${ password }`,
+      registry
+    ];
+    if (verbose) {
+      console.log(['> docker'].concat(args).join(' '));
+    }
+    child.spawn('docker', args, { stdio: 'inherit' })
+      .on('exit', (code, signal) => {
+        if (signal || code) reject(signal || code);
+        else resolve();
+      });
+  });
 }
--- commands/logout.js
+++ commands/logout.js
@@ -1,35 +1,46 @@
-const cli = require('heroku-cli-util')
-const log = require('../lib/log')
+'use strict';
 
-module.exports = function (topic) {
+const cli = require('heroku-cli-util');
+const co = require('co');
+const child = require('child_process');
+
+module.exports = function(topic) {
   return {
     topic: topic,
     command: 'logout',
-    flags: [{name: 'verbose', char: 'v', hasValue: false}],
-    description: 'logout from the Heroku Container Registry',
+    flags: [{ name: 'verbose', char: 'v', hasValue: false }],
+    description: 'Logs out from the Heroku Docker registry',
     needsApp: false,
     needsAuth: false,
-    run: cli.command(logout)
-  }
-}
+    run: cli.command(co.wrap(logout))
+  };
+};
 
-async function logout (context, heroku) {
-  let herokuHost = process.env.HEROKU_HOST || 'heroku.com'
-  let registry = `registry.${ herokuHost }`
+function* logout(context, heroku) {
+  let herokuHost = process.env.HEROKU_HOST || 'heroku.com';
+  let registry = `registry.${ herokuHost }`;
 
   try {
-    let user = await dockerLogout(registry, context.flags.verbose)
+    let user = yield dockerLogout(registry, context.flags.verbose);
   }
   catch (err) {
-    cli.error(`Error: docker logout exited with ${ err }`)
+    cli.error(`Error: docker logout exited with ${ err }`);
   }
 }
 
-function dockerLogout (registry, verbose) {
-  let args = [
-    'logout',
-    registry
-  ]
-  log(verbose, args)
-  return Sanbashi.cmd('docker', args)
+function dockerLogout(registry, verbose) {
+  return new Promise((resolve, reject) => {
+    let args = [
+      'logout',
+      registry
+    ];
+    if (verbose) {
+      console.log(['> docker'].concat(args).join(' '));
+    }
+    child.spawn('docker', args, { stdio: 'inherit' })
+      .on('exit', (code, signal) => {
+        if (signal || code) reject(signal || code);
+        else resolve();
+      });
+  });
 }
